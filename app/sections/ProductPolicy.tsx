25b13505ca856ee59c37384171c71a93ecf7d3d6
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2026-05-08 15:09:58 +0300

    update

--- app/sections/ProductPolicy.tsx
+++ app/sections/ProductPolicy.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 
 export default function ProductPolicy() {
-  return <section className="product-policy py-52">
-   <div className="w-(--container) mx-auto">
-    <div>
-        <h2 className="headingh2 product-policy__headingh2">
-          <p>  <span className="common-txt-gradient inline-block">Two</span>
-            <span className="common-txt-gradient inline-block">Products.</span></p>
-           <p>
-             <span className="common-txt-gradient inline-block"> One </span>
-            <span className="common-txt-gradient inline-block">Policy</span>
-             <span className="common-txt-gradient inline-block">Layer.</span>
-           </p>
-        </h2>
-    </div>
-   </div>
-  </section>
+  return (
+    <section className="product-policy py-52">
+      <div className="w-(--container) mx-auto">
+        <div>
+          <h2 className="headingh2 product-policy__headingh2">
+            <p>
+              {" "}
+              <span className="common-txt-gradient inline-block">Two</span>
+              <span className="common-txt-gradient inline-block">
+                Products.
+              </span>
+            </p>
+            <p>
+              <span className="common-txt-gradient inline-block"> One </span>
+              <span className="common-txt-gradient inline-block py-2">Policy</span>
+              <span className="common-txt-gradient inline-block py-2">Layer.</span>
+            </p>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 }
