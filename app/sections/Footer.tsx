e57ed690cef2444954652b90da33ca0374961c44
Ahmed-Mohamed-Egypt <ahmedatyabas@gmail.com>
2026-05-11 18:30:06 +0300

    update

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -1,16 +1,111 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <section className='inpolicy_footer'>
-        <div className='w-(--container)'>
+    <section className="inpolicy_footer bg-black pt-20 overflow-hidden">
+      <div className="w-(--container) mx-auto px-20">
+        <div className="flex justify-between">
+          <div className="inpolicy_footer--leftside w-[450px]">
+            <img width="125" height="42" src="/Logo.svg" alt="logo" />
+            <p className="font-inter text-grey mt-6">
+              InPolicy turns your policies into active, real-time guardrails. It
+              uses AI to check what employees write in email and chat, instantly
+              flags violations, explains the issue, and provides a one-click
+              fix. Browser extension + Google docs agent.
+            </p>
+          </div>
+          <div className="flex inpolicy_footer--rightside ">
             <div>
-                <div>
-                    <img width="125" height="42" src="/Logo.svg" alt="logo"/>
-                </div>
+              <h3 className="">Legal</h3>
+              <ul>
+                <li>
+                  <a href="#" className="">
+                    [Link]
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className=" ">
+                    [Link]
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    [Link]
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    [Link]
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    [Link]
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    [Link]
+                  </a>
+                </li>
+              </ul>
             </div>
-
+            <div className="mx-20">
+              <h3 className="">Pages</h3>
+              <ul>
+                <li>
+                  <a href="#" className="">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    How it Works
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    Pricing
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    FAQ
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="">Useful</h3>
+              <ul>
+                <li>
+                  <a href="#" className="">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="">
+                    Terms of Services
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <div className="flex justify-between inpolicy_footer--bottomside border border-drkgreen border-r-0 border-l-0 pt-5">
+            <div>
+                <ul className="flex items-center">
+                    <li><a href="#" className="">Terms</a></li>
+                    <li className="ml-6 "><a href="#">Privacy</a></li>
+                </ul>
+            </div>
+            <p className="font-inter text-[14px] text-grey">© InPolicy LLC 2026 All rights reserved.</p>
+        </div>
+      </div>
+      <p className="font-inter font-medium text-[470px] inpolicy_footer--bigText">
+        <span className="common-txt-gradient">In</span>
+        <span className="common-txt-gradient">Policy</span>
+      </p>
     </section>
-  )
+  );
 }
